2f8a4d128ebc0c8c1eefb58ed5c4b4dcd40f260b
El_cor <histopat.cor@gmail.com>
2024-05-23 00:51:00 -0400

    modified:   7_ciclos.js

--- 7_ciclos.js
+++ 7_ciclos.js
@@ -55,3 +55,119 @@ function contarDeDosEnDos (numero) {
 contarDeDosEnDos(5)
 
 console.log("-----")
+
+
+// Ejercicio
+// Crea una función llamada sumarHasta que reciba un número como parámetro.
+
+// La función debe retornar la suma de los números del 1 al número ingresado.
+
+// Por ejemplo, si el número ingresado es 5, la función debe retornar 15. Si el número ingresado es 10, la función debe retornar 55.
+function sumarHasta (numero) {
+    contador = 1
+    suma = 0
+    while (contador <= numero) {
+        suma = suma + contador
+        contador = contador + 1
+    }
+    return suma
+}
+
+console.log(sumarHasta(5))
+
+console.log("-----")
+
+// Ejercicio
+// Crea una función llamada sumarDesdeHasta que reciba dos números como parámetros y retorne la suma de los números desde el primer número hasta el segundo número.
+
+function sumarDesdeHasta (numero1, numero2) {
+    contador = numero1
+    suma = 0
+    while (contador <= numero2) {
+        suma = suma + contador
+        contador = contador + 1
+    }
+    return suma
+}
+console.log(sumarDesdeHasta(1,5))
+
+console.log("-----")   
+
+// Crea una función llamada sumarDeDosEnDos que reciba dos parámetros, desde y hasta. La función debe retornar la suma de los números.
+
+// Ejemplo
+// sumarDeDosEnDos(5, 10) // 5 + 7 + 9 = 21
+// sumarDeDosEnDos(10, 20) // 10 + 12 + 14 + 16 + 18 + 20 = 90
+// sumarDeDosEnDos(3, 7) // 3 + 5 + 7 = 15
+function sumarDeDosEnDos (desde, hasta) {
+    contador = desde
+    suma = 0
+    while (contador <= hasta) {
+        suma = suma + contador
+        contador = contador + 2
+    }
+    return suma
+}
+console.log(sumarDeDosEnDos(5,10))
+
+console.log("-----")
+
+
+// Crea una función llamada productoria que reciba un número como parámetro y retorne la productoria de los números del 1 al número ingresado.
+
+// Por ejemplo, si el número ingresado es 5, la función debe retornar 120. Si el número ingresado es 10, la función debe retornar 3628800.
+function productoria (numero) {
+    contador = 1
+    producto = 1
+    while (contador <= numero) {
+        producto = producto * contador
+        contador = contador + 1
+    }
+    return producto
+}
+
+console.log(productoria(5))
+
+console.log("-----")
+
+// Ejercicio
+// Crea una función llamada introduccionAFor que muestre en consola los números del 1 al 15 utilizando la instrucción for.
+function introduccionAFor () {
+    for (contador = 1; contador <=15; contador += 1) {
+        console.log(contador)
+    }
+
+}
+introduccionAFor()
+
+console.log("-----")
+
+
+// Crea la función mostrarNumeros que reciba dos parámetro, inicio y fin.
+// La función debe mostrar en consola todos los números desde el inicio hasta el fin.
+function mostrarNumeros (inicio, fin) {
+    for (contador = inicio; contador <= fin; contador += 1) {
+        console.log(contador)
+    }
+}
+
+mostrarNumeros(4,9)
+
+// Ejercicio
+// Modificando sólo la línea de instrucción de for, cambia el valor de la variable contador para que muestre en consola los números impares del 1 al 10.
+for (contador = 1; contador <= 10; contador = contador + 2) {
+    console.log(contador)
+}
+
+console.log("-----------------------------------")
+
+
+function sumatoriaPares (numero) {
+    suma = 1
+    for (contador = 1; contador <= numero; contador + 1) {
+        suma += contador
+    }
+    console.log(suma)
+}
+
+sumatoriaPares(5)
